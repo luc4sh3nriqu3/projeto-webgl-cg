9c5c7ca2a38f9d1b26a19d6dcc964df6a8fd4c18
Lucas Freire de Siqueira <85368979+LKSFDS@users.noreply.github.com>
2025-11-23 21:26:46 -0300

    Create bird.js
    add the bird element to the engine

---
+++ src/engine/geometry/bird.js
@@ -0,0 +1,21 @@
+function drawBird() {
+    let angleX = 0; // 30 graus - rotação em X
+    let angleY = 0; // 30 graus - rotação em Y
+
+    //Perna 1
+    const asa1 = createCube('BLACK', 0.2, [0, 0, -0.4]);
+    draw3D(asa1.vertices, asa1.colors, asa1.indices, getTransformMatrix(angleX, angleY, 0.8));
+
+    //Perna 2
+    const asa2 = createCube('BLACK', 0.2, [0, 0.0, 0.4]);
+    draw3D(asa2.vertices, asa2.colors, asa2.indices, getTransformMatrix(angleX, angleY, 0.8));
+    //Corpo
+    const corpo = createCube('BLUE', 0.6, [0.0, 0.0, 0.0], 0.5);
+    draw3D(corpo.vertices, corpo.colors, corpo.indices, getTransformMatrix(angleX, angleY, 0.8));
+
+    const cabeca = createSphere('YELLOW', 0.3, [-0.4, 0.0, 0.0],2);
+    draw3D(cabeca.vertices, cabeca.colors, cabeca.indices, getTransformMatrix(angleX, angleY, 0.8));
+
+    const cauda = createCube('GREEN', 0.2, [0.4, 0.0, 0.0]);
+    draw3D(cauda.vertices, cauda.colors, cauda.indices, getTransformMatrix(angleX, angleY, 0.8));
+}
